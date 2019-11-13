d0db07d0a41d92b1b3fb6ea24d32fe3834a18fbb
Paweł Wilkosz <pwilkosz5@gmail.com>
2019-11-13 22:59:53 +0100

    Remove frontend hosting from backend

--- backend/src/main.go
+++ backend/src/main.go
@@ -8,25 +8,11 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	http.ServeFile(w, r, "home.html")
-}
-
 func main() {
 	flag.Parse()
 	game := newGame()
 	go game.run()
-	http.HandleFunc("/", serveHome)
+
 	http.HandleFunc("/screenWs", func(w http.ResponseWriter, r *http.Request) {
 		serveScreenWs(game, w, r)
 	})
